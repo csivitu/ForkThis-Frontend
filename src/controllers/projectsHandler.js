6af4e801d8df0134f13a5d58e35bdc38266c0faa
Pratham_Mishra04 <prathamm0409@gmail.com>
2022-09-30 15:14:22 +0530

    feat: envs fixes

--- src/controllers/projectsHandler.js
+++ src/controllers/projectsHandler.js
@@ -1,20 +1,16 @@
 import React from "react";
-
 import { Octokit } from "octokit";
 
 const projectsHandler = async (user, repo) => {
-  const octokit = new Octokit({
-    // auth: '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'
-  });
 
-  // const res= await octokit.request('GET /orgs/{org}/repos', {
-  //   org: 'csivitu'
-  // })
+  const octokit = new Octokit({
+    auth: `Bearer ${process.env.REACT_APP_GITHUB_AUTH_TOKEN}`
+  });
   const res = await octokit.request("GET /repos/{user}/{repo}", {
     user: user,
     repo: repo,
   });
-  const URL = "https://api.github.com/repos/Pratham-Mishra04/interact-old";
+
   return res.data;
 };
 
